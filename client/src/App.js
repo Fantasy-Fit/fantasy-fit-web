651042c39b971281922a57489d85a928fc4d9497
Dulat <dulat.kussainov@gmail.com>
2023-03-09 15:28:49 -0500

    updated components hierarchy

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import Authorization from "./pages/auth/Authorization";
+import NewCompetition from "./pages/newcompetition/NewCompetition";
 
 function App() {
   return (
-    <div >
+    <div>
       <Routes>
-        <Route path="/" element={<Main/>} />
-        <Route path="/authorization" element={<Authorization/>} />
+        <Route path="/" element={<Main />} />
+        <Route path="/authorization" element={<Authorization />} />
+        <Route path="/new-competition" element={<NewCompetition />} />
       </Routes>
-
     </div>
   );
 }
--- client/src/pages/newcompetition/NewCompetition.js
+++ client/src/pages/newcompetition/NewCompetition.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import Join from './Join'
+import React from "react";
+import Join from "./Join";
+import Create from "./Create";
 
 function NewCompetition() {
   return (
     <div>
-        <p>New Competion Page</p>
-        <p>Join</p>
-        <Join />
-        <p>Create</p>
+      <p>New Competion Page</p>
+      <h1>Join</h1>
+      <Join />
+      <h1>Create</h1>
+      <Create />
     </div>
-  )
+  );
 }
 
-export default NewCompetition
+export default NewCompetition;
