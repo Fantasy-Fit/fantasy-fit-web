98df506794b3b63d50ca59e02da7fc8adb5d7b0a
Alexis Torosina <alexistorosina1@gmail.com>
2023-03-14 10:17:36 -0400

    user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
     has_many :workouts
     has_many :posts
 
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true
+    validates :password, presence: true
+
 end
