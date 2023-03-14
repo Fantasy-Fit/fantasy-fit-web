6064234f1084fda9bc0797113ed62fb0a8d5e0a6
Chris Li <chris.lty07@gmail.com>
2023-03-14 12:28:35 -0400

    testing first endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
+import { useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import Authorization from "./pages/auth/Authorization";
 
 function App() {
+  useEffect(() => {
+    const request = async () => {
+      let req = await fetch('/users')
+      let res = await req.json();
+      if (req.ok) {
+        console.log(res);
+      }
+    }
+    request();
+  }, [])
+
   return (
     <div >
       <Routes>
-        <Route path="/" element={<Main/>} />
-        <Route path="/authorization" element={<Authorization/>} />
+        <Route path="/" element={<Main />} />
+        <Route path="/authorization" element={<Authorization />} />
       </Routes>
 
     </div>
