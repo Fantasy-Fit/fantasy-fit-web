31d2972a2429b024f0fd8b216fcf13a1a747459d
Chris Li <chris.lty07@gmail.com>
2023-03-16 00:21:47 -0400

    competition/leaderboard/:id endpoint

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,2 +1,23 @@
 class CompetitionsController < ApplicationController
+    def leaderboard
+        competition = Competition.find(params[:id])
+        players = competition.users
+        workouts = competition.workouts
+        
+        playerPoints = {}
+        players.map do |player|
+            playerPoints[player.username] = {points: 0, user: player.username}
+        end
+        
+        workouts.each do |workout| 
+            user = workout.user.username
+            playerPoints[user][:points] += workout.points
+        end
+
+        result = playerPoints.values.sort_by{|h| -h[:points]}.each_with_index do |hash, index|
+            hash[:rank] = index + 1
+        end
+
+        render json: result
+    end
 end
