d05bffb9e711b1961e911d19d3b5cc5640b800c8
Chris Li <chris.lty07@gmail.com>
2023-03-23 12:08:45 -0400

    jwt logout by blacklisting tokens

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
+
     before_action :authenticate_request
 
     private
@@ -12,7 +13,10 @@ class ApplicationController < ActionController::API
         header = request.headers["Authorization"]
         header = header.split(" ").last if header
         decoded = jwt_decode(header)
-        current_user = User.find(decoded[:user_id])
+        @current_user = User.find(decoded[:user_id])
+        @current_user_token = @current_user.user_token.find_by(token: header)
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+        render json: { error: "Unauthorized" }, status: unauthorized
     end
 
 
