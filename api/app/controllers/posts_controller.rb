85443219f955dbf2627e2edfca11a697b4afe8d1
Dulat <dulat.kussainov@gmail.com>
2023-05-01 16:03:54 -0400

    added authenticate to post controller

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -1,19 +1,22 @@
 class PostsController < ApplicationController
 
+    before_action :authenticate_request, only: [:create, :destroy]
+
     def index
         competition = Competition.find(params[:competition_id])
         render json: competition.posts, status: :ok
     end
 
     def create
-        user = User.find(params[:user_id])
+        user = @current_user
         competition = Competition.find(params[:competition_id])
-        post = Post.create!(post_params)
+        post = Post.create!(user_id: user.id, competition_id: competition.id, description: params[:description])
         render json: post, status: :created
     end
 
     def destroy
-        post = Post.find(params[:id])
+        user = @current_user
+        post = user.posts.find(params[:id])
         post.destroy
         head :no_content
     end
