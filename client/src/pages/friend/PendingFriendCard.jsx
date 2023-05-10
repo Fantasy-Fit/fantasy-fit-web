2e0d0691753c4475e130ebe53604d479577b83f6
Chris Li <chris.lty07@gmail.com>
2023-05-09 23:18:31 -0400

    continue styling for mobile responsiveness

--- client/src/pages/friend/PendingFriendCard.jsx
+++ client/src/pages/friend/PendingFriendCard.jsx
@@ -28,7 +28,7 @@ const PendingFriendCard = ({ friendship, refetchFriends }) => {
     return (
         <div>
             <h2>{friendship.friend_username}</h2>
-            <img src={friendship.friend_avatar} alt="friendicon" />
+            <img src={friendship.friend_avatar || "https://static.vecteezy.com/system/resources/previews/009/734/564/original/default-avatar-profile-icon-of-social-media-user-vector.jpg"} alt="friendicon" />
             <p>Request received: {friendship.created_at}</p>
             <p>Status: {friendship.status}</p>
             <button onClick={handleAcceptRequest}>Accept</button>
