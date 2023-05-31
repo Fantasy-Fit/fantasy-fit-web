8a64e29b67d0713a02c410e195f63cd123682661
Chris Li <chris.lty07@gmail.com>
2023-05-31 14:38:19 -0400

    update origins in cors.rb

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 
-    "http://localhost:4000",
+    origins "http://localhost:4000",
     "https://fantasyfit.netlify.app", 
     "https://main--fantasyfit.netlify.app",
-    /https:\/\/deploy-preview-\d{1,3}--fantasyfit\.netlify\.app/
+    /\Ahttps:\/\/deploy-preview-\d{1,4}--fantasyfit\.netlify\.app/\z
 
     resource "*",
       headers: :any,
