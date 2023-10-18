4c3c7d0f08c9689d58449f6d2d48ff2ea76f4a37
Chris Li <chris.lty07@gmail.com>
2023-10-18 14:29:22 -0400

    react syntax changes for bootstrap jsx

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -3,33 +3,33 @@ import "./Carousel.css";
 
 function Carousel() {
   return (
-    <div class="300-px-wide">
-    <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-        <img src="https://www.sliderrevolution.com/wp-content/uploads/2023/04/gym-website-templates-2.gif" class="d-block w-100" alt="..."/>
+    <div className="300-px-wide">
+      <div id="carouselExampleIndicators" className="carousel slide">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-        <div class="carousel-item">
-        <img src="https://images.pexels.com/photos/4325466/pexels-photo-4325466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="..."/>
+        <div className="carousel-inner" role="listbox">
+          <div className="carousel-item active">
+            <img src="https://www.sliderrevolution.com/wp-content/uploads/2023/04/gym-website-templates-2.gif" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="https://images.pexels.com/photos/4325466/pexels-photo-4325466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="https://images.pexels.com/photos/1289107/pexels-photo-1289107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="..." />
+          </div>
         </div>
-        <div class="carousel-item">
-        <img src="https://images.pexels.com/photos/1289107/pexels-photo-1289107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="..."/>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-    </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   )
 }
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -15,68 +15,68 @@ function Main() {
   }
   return (
     <div>
-      <div class="container-fluid">
-        <NavBar/>
+      <div className="container-fluid">
+        <NavBar />
       </div>
-      <div class="row">
-        <Carousel/>
+      <div className="row">
+        <Carousel />
       </div>
-      <div class="container-xxl">
-        <div class="row mt-5">
-          <div class="col-6">
-            <img src="https://img.freepik.com/free-photo/outdoor-shot-active-dark-skinned-man-running-morning-has-regular-trainings-dressed-tracksuit-comfortable-sneakers-concentrated-into-distance-sees-finish-far-away_273609-29401.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697500800&semt=sph" class="rounded img-fluid" alt="..."/>
+      <div className="container-xxl">
+        <div className="row mt-5">
+          <div className="col-6">
+            <img src="https://img.freepik.com/free-photo/outdoor-shot-active-dark-skinned-man-running-morning-has-regular-trainings-dressed-tracksuit-comfortable-sneakers-concentrated-into-distance-sees-finish-far-away_273609-29401.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697500800&semt=sph" className="rounded img-fluid" alt="..." />
           </div>
-          <div class="col-6 bg-light rounded">
-            <h2 class="text-center">Create and Join Fitness Competitions</h2>
-            <p class="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
-            <button type="button" class="btn btn-warning">Sign Up</button>
-          </div>
-        </div>
-        <div class="row mt-5">
-          <div class="col-6 bg-light rounded">
-            <h2 class="text-center">Upload Your Workouts</h2>
-            <p class="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
-            <button type="button" class="btn btn-warning">Sign Up</button>
-          </div>
-          <div class="col-6">
-            <img src="https://prod-ne-cdn-media.puregym.com/media/819394/gym-workout-plan-for-gaining-muscle_header.jpg?quality=80" class="rounded img-fluid" alt="..."/>
+          <div className="col-6 bg-light rounded">
+            <h2 className="text-center">Create and Join Fitness Competitions</h2>
+            <p className="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
+            <button type="button" className="btn btn-warning">Sign Up</button>
           </div>
         </div>
-        <div class="row mt-5">
-          <div class="col-6">
-            <img src="https://media.self.com/photos/6398b36c72eb56f726777d06/4:3/w_2560%2Cc_limit/weekly-workout-schedule.jpeg" class="rounded img-fluid" alt="..."/>
+        <div className="row mt-5">
+          <div className="col-6 bg-light rounded">
+            <h2 className="text-center">Upload Your Workouts</h2>
+            <p className="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
+            <button type="button" className="btn btn-warning">Sign Up</button>
           </div>
-          <div class="col-6 bg-light rounded">
-            <h2 class="text-center">Track Your Progress On Leaderboard</h2>
-            <p class="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
-            <button type="button" class="btn btn-warning">Sign Up</button>
-          </div>
-        </div>
-        <div class="row mt-5">
-          <div class="col-6 bg-light rounded">
-            <h2 class="text-center">Share With Your Friends</h2>
-            <p class="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
-            <button type="button" class="btn btn-warning">Sign Up</button>
-          </div>
-          <div class="col-6">
-            <img src="https://blog.anytimefitness.co.uk/wp-content/uploads/2019/06/2019_214_females_functional_high_five-5078x2539.jpg" class="rounded img-fluid" alt="..."/>
+          <div className="col-6">
+            <img src="https://prod-ne-cdn-media.puregym.com/media/819394/gym-workout-plan-for-gaining-muscle_header.jpg?quality=80" className="rounded img-fluid" alt="..." />
           </div>
         </div>
-        <div class="row mt-5">
-          <div class="col-6">
-            <img src="https://media.istockphoto.com/id/682150858/photo/marathon-runners-close-up-legs-and-shoes.jpg?s=612x612&w=0&k=20&c=NqbMBEhyOvp1hd6H12IiijavdEeWWuEiCslYhUOa3ik=" class="rounded img-fluid" alt="..."/>
+        <div className="row mt-5">
+          <div className="col-6">
+            <img src="https://media.self.com/photos/6398b36c72eb56f726777d06/4:3/w_2560%2Cc_limit/weekly-workout-schedule.jpeg" className="rounded img-fluid" alt="..." />
           </div>
-          <div class="col-6 bg-light rounded">
-            <h2 class="text-center">Play for Fun or For Charity</h2>
-            <p class="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
-            <button type="button" class="btn btn-warning">Sign Up</button>
+          <div className="col-6 bg-light rounded">
+            <h2 className="text-center">Track Your Progress On Leaderboard</h2>
+            <p className="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
+            <button type="button" className="btn btn-warning">Sign Up</button>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col-6 bg-light rounded">
+            <h2 className="text-center">Share With Your Friends</h2>
+            <p className="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
+            <button type="button" className="btn btn-warning">Sign Up</button>
+          </div>
+          <div className="col-6">
+            <img src="https://blog.anytimefitness.co.uk/wp-content/uploads/2019/06/2019_214_females_functional_high_five-5078x2539.jpg" className="rounded img-fluid" alt="..." />
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col-6">
+            <img src="https://media.istockphoto.com/id/682150858/photo/marathon-runners-close-up-legs-and-shoes.jpg?s=612x612&w=0&k=20&c=NqbMBEhyOvp1hd6H12IiijavdEeWWuEiCslYhUOa3ik=" className="rounded img-fluid" alt="..." />
+          </div>
+          <div className="col-6 bg-light rounded">
+            <h2 className="text-center">Play for Fun or For Charity</h2>
+            <p className="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam arcu, pellentesque sit amet elementum id, ultrices non mi. Donec erat sem, condimentum nec elit sit amet, maximus volutpat augue. Etiam diam ante, feugiat eu dolor nec, condimentum congue tortor. Nulla dictum ut erat vitae fringilla. Phasellus ultricies ex at porta vestibulum. Phasellus lacinia malesuada orci, vel egestas ipsum blandit vel. Duis vulputate aliquam leo, sed lacinia lorem accumsan sed. Vestibulum vulputate purus et tortor porta, at ullamcorper quam mollis. Sed eu dui at velit luctus dignissim vitae sit amet diam.</p>
+            <button type="button" className="btn btn-warning">Sign Up</button>
           </div>
         </div>
       </div>
-      <div class="container-fluid text-center pt-5">
+      <div className="container-fluid text-center pt-5">
         <h2>Current Competitions</h2>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
